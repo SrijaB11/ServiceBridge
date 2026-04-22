e9e8a10b006c4e4fab84e33cfbde2bd5c9df459a
vamshielluru <vamshielluru@gmail.com>
2026-04-22 11:06:45 +0530

    vamshi

---
+++ backend/models/UserModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+let userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    number:String,
+    password:String,
+    role:String
+})
+
+let UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
