a4d9613903eba5be3ca13eab3883082adb24b2b6
Deepak <deepakmahesh55@gmail.com>
2026-04-23 12:12:40 +0530

    my local changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -23,6 +23,40 @@ router.post("/register",async (req,res)=>{
         
 })
 
+
+
+router.post("/login",async(req,res)=>{ 
+        let data=req.body;
+        try{
+            let result= await userModel.findOne({email:data.email})
+            // console.log(result);
+            // console.log(process.env.JWT_SECRET);
+            if(!result){
+                return res.status(401).json({
+                        message:"Invalid email"
+                })
+            }
+            // console.log(result);
+            if(result.password !== data.password){
+                return res.status(401).json({
+                        message:"Wrong password"
+                })
+            }
+            let token =jwt.sign({id:result._id,role:result.role},process.env.JWT_SECRET,{expiresIn:"1d"})
+            res.status(200).json({
+                message:"Successfully login",
+                token:token
+            })
+        }
+        catch(err){
+            //console.log(err)
+            res.status(500).json({
+                message:"Unsuccessfull login"
+            })
+        }
+
+})
+
    
 
 module.exports = router;
