d7f97e71ac869df8f5c875cac6333eae2a8564ef
Deepak <deepakmahesh55@gmail.com>
2026-04-24 23:31:53 +0530

    customendashboardfe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Home from "./Pages/Landing/Home";
+import CustomerDashboard from "./Pages/Customer/CustomerDashboard"
 
 function App() {
   return (
