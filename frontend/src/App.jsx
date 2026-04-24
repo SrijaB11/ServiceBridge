b90e707c82104f180c766954656945114e34f955
SrijaB11 <boddusrija11@gmail.com>
2026-04-24 10:30:05 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Home from "./Pages/Landing/Home";
 
 function App() {
   return (
-    <div>
-      <Register />
-      <Login />
-      {/* <Home /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
