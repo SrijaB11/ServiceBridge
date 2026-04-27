78ea3bc43956876666de3192a772960519019aba
Deepak <deepakmahesh55@gmail.com>
2026-04-27 10:17:38 +0530

    deleted customer dashbord

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,8 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
-<<<<<<< HEAD
 const workerRoutes = require("./routes/customer/workersListRoutes");
-=======
 const adminWorkerRoutes = require("./routes/adminWorkerRoutes");
->>>>>>> 4175edaccfb49ec90591fc9fe4b733af5fbe3960
 
 const app = express();
 
@@ -15,16 +12,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
+
+
 app.use("/",userRoutes);
-app.use("/customer",workerRoutes)
-=======
->>>>>>> 4175edaccfb49ec90591fc9fe4b733af5fbe3960
-
-app.use(cors());
-app.use(express.json()); 
-
-app.use("/", userRoutes);
+app.use("/customer",workerRoutes);
 app.use("/admin", adminWorkerRoutes);
 
 app.listen(5000, () => {
