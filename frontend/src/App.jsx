f1fa31d5d9aae02a8038860b2033f4d22b2fa44f
SrijaB11 <boddusrija11@gmail.com>
2026-04-30 12:30:41 +0530

    checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import ActiveJobs from "./Pages/WorkersDashboard/ActiveJobs";
 import NavBar from "./Pages/WorkersDashboard/Navbar";
 import Header from "./Pages/WorkersDashboard/Header";
 import Requests from "./Pages/WorkersDashboard/Requests";
+import WorkersDashboard from "./Pages/WorkersDashboard/Dashboard";
 
 // Admin Dashboard Components
 import AdminNavBar from "./Pages/AdminDashboard/Navbar";
@@ -24,7 +25,6 @@ import RecentRequests from "./Pages/AdminDashboard/RecentRequests";
 import RecentComplaints from "./Pages/AdminDashboard/RecentComplaints";
 
 import "./App.css";
-import WorkersDashboard from "./Pages/WorkersDashboard/Dashboard";
 
 // Worker Dashboard Layout Component
 function WorkerDashboardLayout() {
@@ -117,126 +117,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Toaster } from "react-hot-toast";
-
-// import Login from "./Pages/Login";
-// import Home from "./Pages/Home";
-// import Register from "./Pages/Register/Register";
-
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// // Customer
-// import CustomerDashboard from "./Pages/Customer/CustomerDashboard";
-// import ServiceProviders from "./components/customer/ServiceProviders";
-
-// // Worker
-// import WorkersDashboard from "./Pages/WorkersDashboard/WorkersDashboard";
-// import ActiveJobs from "./Pages/WorkersDashboard/ActiveJobs";
-// import Requests from "./Pages/WorkersDashboard/Requests";
-// import NavBar from "./Pages/WorkersDashboard/Navbar";
-// import Header from "./Pages/WorkersDashboard/Header";
-
-// // Admin
-// import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
-// import AdminNavBar from "./Pages/AdminDashboard/Navbar";
-// import AdminHeader from "./Pages/AdminDashboard/Header";
-// import Users from "./Pages/AdminDashboard/Users";
-// import RecentRequests from "./Pages/AdminDashboard/RecentRequests";
-// import RecentComplaints from "./Pages/AdminDashboard/RecentComplaints";
-
-// import "./App.css";
-
-// // 🔧 Worker Layout
-// function WorkerDashboardLayout() {
-//   return (
-//     <>
-//       <Header />
-//       <div className="app-layout flex">
-//         <NavBar />
-//         <div className="main-content flex-1 p-4">
-//           <Routes>
-//             <Route index element={<WorkersDashboard />} />
-//             <Route path="requests" element={<Requests />} />
-//             <Route path="active-jobs" element={<ActiveJobs />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// // 🔧 Admin Layout
-// function AdminDashboardLayout() {
-//   return (
-//     <>
-//       <AdminHeader />
-//       <div className="app-layout flex">
-//         <AdminNavBar />
-//         <div className="main-content flex-1 p-4">
-//           <Routes>
-//             <Route index element={<AdminDashboard />} />
-//             <Route path="recent-requests" element={<RecentRequests />} />
-//             <Route path="recent-complaints" element={<RecentComplaints />} />
-//             <Route path="users" element={<Users />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// // 🌐 Main App
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Toaster position="top-right" />
-
-//       <Routes>
-//         {/* 🌍 Public */}
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-
-//         {/* 👤 Customer */}
-//         <Route
-//           path="/customer/*"
-//           element={
-//             <ProtectedRoute role="customer">
-//               <CustomerDashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/service/:id"
-//           element={
-//             <ProtectedRoute role="customer">
-//               <ServiceProviders />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         {/* 🧑‍🔧 Worker */}
-//         <Route
-//           path="/worker/*"
-//           element={
-//             <ProtectedRoute role="worker">
-//               <WorkerDashboardLayout />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         {/* 🛠️ Admin */}
-//         <Route
-//           path="/admin/*"
-//           element={
-//             <ProtectedRoute role="admin">
-//               <AdminDashboardLayout />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
