7063d83aa88f123652e2e4327be0b2ea1c2fcbb9
Deepak <deepakmahesh55@gmail.com>
2026-05-11 11:10:51 +0530

    just pulling

---
+++ backend/middlewares/customerMiddleware.js
@@ -0,0 +1,22 @@
+const customerMiddleware = async (req, res, next) => {
+  try {
+
+    // role check
+    if (req.role !== "customer") {
+      return res.status(403).json({
+        message: "Only customers allowed",
+      });
+    }
+
+    next();
+
+  } catch (error) {
+    //console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = customerMiddleware;
