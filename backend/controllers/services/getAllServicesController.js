51cb9902990aa11335cd6330b0d9c76e4ff569b0
Deepak <deepakmahesh55@gmail.com>
2026-05-14 20:43:39 +0530

    databaseservice

--- backend/controllers/services/getAllServicesController.js
+++ backend/controllers/services/getAllServicesController.js
@@ -1,19 +1,23 @@
-const services = require("../../services/services");
+const Service = require("../../models/ServiceModel");
 
 const getAllServicesController = async (req, res) => {
   try {
 
+    const services = await Service.find();
+
     res.status(200).json({
+      success: true,
       message: "Services fetched successfully",
       totalServices: services.length,
       services,
     });
 
   } catch (error) {
-    //console.log("Get Services Error:", error);
 
     res.status(500).json({
+      success: false,
       message: "Server Error",
+      error: error.message,
     });
   }
 };
