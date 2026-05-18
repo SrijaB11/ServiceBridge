cd88fb81f780b759c48795838562c6b1bb8a311b
Deepak <deepakmahesh55@gmail.com>
2026-05-18 12:17:03 +0530

    just pulling frontend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import workerReducer from './slices/workerSlice';
+
+const store = configureStore({
+    reducer: {
+        workers: workerReducer,
+    },
+});
+
+export default store;
