e10cf22eb24d57feb9c4b3efc1621a4743e579da
Sai Teja <mintusaiteja0068@gmail.com>
2026-05-20 09:13:57 +0530

    commit

--- frontend/src/Pages/AdminDashboard/CustomerPayments/index.jsx
+++ frontend/src/Pages/AdminDashboard/CustomerPayments/index.jsx
@@ -7,7 +7,7 @@ class CustomerPayments extends Component {
     }
     GetCustomerPaymentDetails = async () => {
         const JwtToken=localStorage.getItem("token")
-        const PaymentDetails = await fetch("http://localhost:5000/payment/verify-payment",{method:"GET",headers:{Authorization:`Bearer ${JwtToken}`}})
+        const PaymentDetails = await fetch("http://localhost:5000/payment/create-order",{method:"POST",headers:{Authorization:`Bearer ${JwtToken}`}})
         console.log(PaymentDetails)
     }
     render() {
