aaa17579859161499b66a10103f5456d29074770
Deepak <deepakmahesh55@gmail.com>
2026-05-21 23:56:07 +0530

    appRatingGet

--- backend/routes/apprating/appRating.js
+++ backend/routes/apprating/appRating.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const customerRatingController = require("../../controllers/apprating/customerRatingController");
-
+const getCustomerAppReviewController = require("../../controllers/apprating/getCustomerAppReviewController");
 const getOverallRatingController = require("../../controllers/apprating/getOverallRatingController");
 
 const authMiddleware = require("../../middlewares/authMiddleware");
@@ -14,5 +14,8 @@ router.post("/rate",authMiddleware,customerRatingController);
 
 // Get overall app rating
 router.get("/overall",authMiddleware,getOverallRatingController);
+
+//Customer sees his own review
+router.get("/customerappreview",authMiddleware,getCustomerAppReviewController);
 
 module.exports = router;
