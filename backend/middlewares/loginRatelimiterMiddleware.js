e6faf9fb0ee3845d36d75bde1c9d53f34e3e3892
Deepak <deepakmahesh55@gmail.com>
2026-05-29 10:36:07 +0530

    login uncomment

--- backend/middlewares/loginRatelimiterMiddleware.js
+++ backend/middlewares/loginRatelimiterMiddleware.js
@@ -1,9 +1,9 @@
 const rateLimit = require( "express-rate-limit");
 
 const loginRateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 mins
+    windowMs: 1 * 60 * 1000, // 1 mins
     max: 5, // only 5 login attempts
-    message: { message: "Too many login attempts. Try again after 15 minutes",},
+    message: { message: "Too many login attempts. Try again after 1 minutes",},
 
     standardHeaders: true,
 
