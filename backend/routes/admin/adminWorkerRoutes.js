a4ffd7d8ac9f08d902b399ffa323dca1dfb280de
vamshielluru <vamshielluru@gmail.com>
2026-05-29 18:10:44 +0530

    abcd

--- backend/routes/admin/adminWorkerRoutes.js
+++ backend/routes/admin/adminWorkerRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../../middlewares/authMiddleware");
+const adminMiddleware = require("../../middlewares/adminMiddleware");
 const { getAllWorkers,updateWorker,deleteWorker, } = require("../../controllers/admin/adminWorkerController");
-router.get("/workers", getAllWorkers);
-router.put("/workers/:id", updateWorker);
-router.delete("/workers/:id", deleteWorker);
+
+router.get("/workers", authMiddleware, adminMiddleware, getAllWorkers);
+router.put("/workers/:id",authMiddleware, adminMiddleware, updateWorker);
+router.delete("/workers/:id",authMiddleware,adminMiddleware, deleteWorker);
 
 
 module.exports = router;
