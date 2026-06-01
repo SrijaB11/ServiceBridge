f7e1d3ccd20693ceed1074edd68fa7f5df0f8a44
SrijaB11 <boddusrija11@gmail.com>
2026-06-01 10:23:22 +0530

    customer-routes

--- frontend/src/routes/CustomerRoutes.jsx
+++ frontend/src/routes/CustomerRoutes.jsx
@@ -1,87 +1,3 @@
-// import { Route } from "react-router-dom";
-
-// import ProtectedRoute from "../components/ProtectedRoute";
-
-// import CustomerDashboard from "../Pages/Customer/CustomerDashboard";
-// import DashboardHome from "../components/customer/DashboardHome";
-// import CustomerServices from "../components/customer/CustomerServices";
-// import Bookings from "../components/customer/Bookings";
-// import History from "../components/customer/History";
-// import Profile from "../components/customer/Profile";
-// import ServiceProviders from "../components/customer/ServiceProviders";
-// import BookWorkerPage from "../components/customer/BookWorkerPage";
-// import ComplaintPage from "../components/customer/ComplaintPage";
-
-// import Complaint from "../Pages/Customer/Complaint";
-// import Review from "../pages/Customer/Review";
-// import PaymentPage from "../Pages/PaymentPage";
-
-// export default function CustomerRoutes() {
-//   return (
-//     <>
-//       <Route
-//         path="/customer"
-//         element={
-//           <ProtectedRoute role="customer">
-//             <CustomerDashboard />
-//           </ProtectedRoute>
-//         }
-//       >
-//         <Route index element={<DashboardHome />} />
-
-//         <Route path="services" element={<CustomerServices />} />
-
-//         <Route path="bookings" element={<Bookings />} />
-
-//         <Route path="payment" element={<PaymentPage />} />
-
-//         <Route path="history" element={<History />} />
-
-//         <Route path="complaints" element={<Complaint />} />
-
-//         <Route path="profile" element={<Profile />} />
-
-//         <Route path="review" element={<Review />} />
-//       </Route>
-
-//       <Route
-//         path="/service/:id"
-//         element={
-//           <ProtectedRoute role="customer">
-//             <ServiceProviders />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       <Route
-//         path="/book-worker/:workerId"
-//         element={
-//           <ProtectedRoute role="customer">
-//             <BookWorkerPage />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       <Route
-//         path="/complaint/:bookingId"
-//         element={
-//           <ProtectedRoute role="customer">
-//             <ComplaintPage />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       <Route
-//         path="/customer-complaints"
-//         element={
-//           <ProtectedRoute role="customer">
-//             <Complaint />
-//           </ProtectedRoute>
-//         }
-//       />
-//     </>
-//   );
-// }
 import { Route, Outlet } from "react-router-dom";
 
 import ProtectedRoute from "../components/ProtectedRoute";
@@ -100,7 +16,7 @@ import Complaint from "../Pages/Customer/Complaint";
 import Review from "../pages/Customer/Review";
 import PaymentPage from "../Pages/PaymentPage";
 
-function CustomerProtectedLayout() {
+function CustomerLayout() {
   return (
     <ProtectedRoute role="customer">
       <Outlet />
@@ -109,16 +25,9 @@ function CustomerProtectedLayout() {
 }
 
 export const CustomerRoutes = (
-  <>
-    {/* Customer Dashboard */}
-    <Route
-      path="/customer"
-      element={
-        <ProtectedRoute role="customer">
-          <CustomerDashboard />
-        </ProtectedRoute>
-      }
-    >
+  <Route element={<CustomerLayout />}>
+    {/* Dashboard */}
+    <Route path="/customer" element={<CustomerDashboard />}>
       <Route index element={<DashboardHome />} />
       <Route path="services" element={<CustomerServices />} />
       <Route path="bookings" element={<Bookings />} />
@@ -129,15 +38,10 @@ export const CustomerRoutes = (
       <Route path="review" element={<Review />} />
     </Route>
 
-    {/* Other Customer Protected Routes */}
-    <Route element={<CustomerProtectedLayout />}>
-      <Route path="/service/:id" element={<ServiceProviders />} />
-
-      <Route path="/book-worker/:workerId" element={<BookWorkerPage />} />
-
-      <Route path="/complaint/:bookingId" element={<ComplaintPage />} />
-
-      <Route path="/customer-complaints" element={<Complaint />} />
-    </Route>
-  </>
+    {/* Customer-only pages */}
+    <Route path="/service/:id" element={<ServiceProviders />} />
+    <Route path="/book-worker/:workerId" element={<BookWorkerPage />} />
+    <Route path="/complaint/:bookingId" element={<ComplaintPage />} />
+    <Route path="/customer-complaints" element={<Complaint />} />
+  </Route>
 );
